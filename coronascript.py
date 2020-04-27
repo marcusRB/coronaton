0ecb525cb8a315201fcf47b0f7e8142354d276ce
Marco <contact.marcusrb@gmail.com>
2020-04-27 18:08:38 +0200

    Update coronascript.py

--- coronascript.py
+++ coronascript.py
@@ -267,10 +267,8 @@ data = df1.join(df2.iloc[:,1:])
 data = data.join(df3.iloc[:,1:])
 
 # Write destination
-import gcsfs
-fs = gcsfc.GCSFileSystem(project=proj_ID)
-with fs.open('gs://aischool_dataoutput/mrusso@paradigmadigital.csv') as f:
-	data.to_csv(f, index=None, encoding="UTF-8", sep=",")
+destination = 'gs://aischool_dataoutput/mrusso@paradigmadigital.csv'
+data.to_csv(destination, index=None, encoding="UTF-8", sep=",")
 #destination = f'gs://coronaton/output/mrusso@paradigmadigital.csv' # dalcon project
 #destination = f'gs://aischool_dataoutput/mrusso@paradigmadigital.csv' #marcusrb prj
 
