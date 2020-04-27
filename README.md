c34548ad65780b8525f2e3a8361ca963dc7c05d8
Marco <contact.marcusrb@gmail.com>
2020-04-27 22:20:29 +0200

    update DASK

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# coronaton
+# CORONATON 2020
  coronaton-docker
  
  Para la ejecucción he realizado la operación de creación del docker directamente desde Container Registry
  
- 
- time docker run gcr.io/aischool-272715/coronascript
+## credential docker en VM
+```
+docker-credential-gcr configure-docker
+```
+
+```
+## execute docker
+time docker run gcr.io/aischool-272715/coronascript
+```
+
--- coronascript.py
+++ coronascript.py
@@ -3,61 +3,46 @@ from google.cloud import bigquery
 # Importamos la librería de cuenta de servicios de BQ
 from google.oauth2 import service_account
 import pandas as pd
-import gcsfs
 import os
 
 
+
 # set envirnoment
-#os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'gs://aischool_dataoutput/key/credentials.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/jovyan/coronaton/key/credentials.json'
 
+# Guardamos la variable proj ID 
+proj_ID = 'aischool-272715'
 
-## set global vars
-
-#proj_ID = 'sandbox-dalcon' # set dalcon proj
-proj_ID = 'aischool-272715' # set marcusRB projectID
-dataID = 'natalidad'
-tableName = "natal"
-
-## set bucket storage input
-# uri = 'gs://coronaton/data/natalidad*' # dalcon-project 
-uri = 'gs://dev_bucket_aischool/natality*' # marcusRB project
-
-## set bucket storage output
-#destination = 'gs://coronaton/output/mrusso@paradigmadigital.csv' # dalcon project
-destination = f'gs://aischool_dataoutput/mrusso@paradigmadigital.csv' #marcusrb prj
-
-## Construct a BigQuery client object. 
-
-#!export GOOGLE_APPLICATION_CREDENTIALS = '/home/jovyan/DS/Challenges/CORONATON/key/credentials.json'
-#creds = service_account.Credentials.from_service_account_file('key/credentials.json')
-#scoped_credentials = creds.with_scopes(
-#    ['https://www.googleapis.com/auth/cloud-platform'])
 #client = bigquery.Client(credentials=creds, project=proj_ID)
 client = bigquery.Client()
-
-
-## CREATE DATASET
 datasets = list(client.list_datasets())  # Make an API request.
 project = client.project
 
-dataset_id = "{0}.{1}".format(client.project, dataID)
 
 def createDataset(dataID, dataset_id):
 
     # Construct a full Dataset object to send to the API.
     dataset = bigquery.Dataset(dataset_id)
-    #Specify the geographic location where the dataset should reside.
+
+    # TODO(developer): Specify the geographic location where the dataset should reside.
     dataset.location = "US"
+
     # Send the dataset to the API for creation.
+    # Raises google.api_core.exceptions.Conflict if the Dataset already
+    # exists within the project.
     dataset = client.create_dataset(dataset)  # Make an API request.
     print("Created dataset {}.{}".format(client.project, dataset.dataset_id))
-    
+
 def deleteDataset(dataset_id):
     client.delete_dataset(dataset_id, 
                           delete_contents=True, not_found_ok=True)
     print("Deleted dataset '{}'.".format(dataset_id))
-    
 
+
+# Construct a BigQuery client object.
+proj_ID = 'aischool-272715'
+dataID = 'natal'
+dataset_id = "{0}.{1}".format(client.project, dataID)
 
 bq_dataset = []
 for dataset in datasets:
@@ -69,7 +54,7 @@ if dataID in bq_dataset:
 else:
     createDataset(dataID,dataset_id)
 
-## CREATE TABLE
+
 dataset_ref = client.dataset(dataID)
 # Configure the load job
 job_config = bigquery.LoadJobConfig(
@@ -80,9 +65,8 @@ job_config = bigquery.LoadJobConfig(
     # The source format defaults to CSV. The line below is optional.
     source_format=bigquery.SourceFormat.CSV
 )
-
-
-
+uri = 'gs://dev_bucket_aischool/natality*'
+tableName = "natal"
 dataset_ref_table = dataset_ref.table(tableName)
 load_job = client.load_table_from_uri(
     uri, dataset_ref_table, job_config=job_config
@@ -92,10 +76,9 @@ print("Starting job {}".format(load_job.job_id))
 load_job.result()  # Waits for table load to complete.
 print("Job finished.")
 
-table = client.get_table(dataset_ref_table)  # Make an API request.  # Make an API request.
+table = client.get_table(dataset_ref_table)  # Make an API request.
 
-
-# Define the query1
+# Define the query
 QUERY_1 = """
     SELECT
             state AS Estado,
@@ -114,14 +97,12 @@ QUERY_1 = """
       state
 """.format(project=table.project, dataset=table.dataset_id, table=table.table_id)
 
-
 # Define the parameter values in a query job configuration
 query_job = client.query(QUERY_1)
 # Return the results as a pandas DataFrame
 df1 = query_job.to_dataframe()
-print("Finish Query_1")
 
-# Define the query2
+# Define the query
 QUERY_2 = """
 WITH Race70 AS(
 SELECT
@@ -225,10 +206,10 @@ Race00 AS(
 	    )
 SELECT
 	r70.Estado70,
-	r70.raza70 as Race70,
-	r80.raza80 as Race80,
-	r90.raza90 as Race90,
-    r00.raza00 as Race00
+	r70.raza70,
+	r80.raza80,
+	r90.raza90,
+	r00.raza00
 FROM (
   SELECT
   ARRAY_AGG(Race70 ORDER BY Race70.counter70 DESC LIMIT 1)[OFFSET(0)] AS r70,
@@ -243,15 +224,13 @@ FROM (
 """.format(project=table.project, dataset=table.dataset_id, table=table.table_id)
 
 
-
 # Define the parameter values in a query job configuration
 query_job = client.query(QUERY_2)
 # Return the results as a pandas DataFrame
 df2 = query_job.to_dataframe()
-print("Finish Query_2")
 
 
-# Define the query3
+# Define the query
 QUERY_3 = """
     SELECT
             state AS Estado,
@@ -268,23 +247,25 @@ QUERY_3 = """
       state
 """.format(project=table.project, dataset=table.dataset_id, table=table.table_id)
 
-
 # Define the parameter values in a query job configuration
 query_job = client.query(QUERY_3)
 # Return the results as a pandas DataFrame
 df3 = query_job.to_dataframe()
-print("Finish Query_3")
 
-# Create dataframe
+## Create dataframe
 data = df1.join(df2.iloc[:,1:])
 data = data.join(df3.iloc[:,1:])
 
-# Write destination
-import dask.dataframe as dd
-ddf = data.from_pandas(data, npartitions=1, sort=True)
-#destination = 'gs://coronaton/output/mrusso@paradigmadigital.csv' # dalcon project
+# Write to GCS
 destination = f'gs://aischool_dataoutput/mrusso@paradigmadigital.csv'
-dd.to_csv(destination, index=None, encoding="UTF-8", sep=",")
+data.to_csv(destination, index=None, encoding="UTF-8", sep=",")
 
 
 
+
+# Write destination
+#import dask.dataframe as dd
+#ddf = dd.from_pandas(data, npartitions=1, sort=True)
+#destination = 'gs://coronaton/output/mrusso@paradigmadigital.csv' # dalcon project
+#destination = f'gs://aischool_dataoutput/mrusso@paradigmadigital-2.csv'
+#ddf.to_csv(destination, index=None, encoding="UTF-8", sep=",")
